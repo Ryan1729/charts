c8078a79ffb3d660d28089b0eb6bf747cee27dfa
Ryan1729 <Ryan1729@gmail.com>
2025-02-28 00:06:00 -0700

    support tuples

--- src/Parse.elm
+++ src/Parse.elm
@@ -20,6 +20,12 @@ parse input =
     let
         acc =
             String.foldl parseStep accInit input
+
+        extractOutput : Acc -> Output
+        extractOutput =
+            .output
+                >> List.sortBy .x
+                >> Ok
     in
     case acc.kind of
         ErrorOut error ->
@@ -27,19 +33,22 @@ parse input =
 
         ExpectDigit last ->
             appendNumber last acc
-                |> .output
-                |> List.reverse
-                |> Ok
+                |> extractOutput
+
+        ExpectY xNumber yNumber ->
+            appendPoint xNumber yNumber acc
+                |> extractOutput
 
         _ ->
-            acc.output
-                |> List.reverse
-                |> Ok
+            acc
+                |> extractOutput
 
 
 type Kind
     = Initial
     | ExpectDigit Int
+    | ExpectX Int
+    | ExpectY Int Int
     | ErrorOut Error
 
 
@@ -56,8 +65,8 @@ accInit =
     }
 
 
-setKind : Acc -> Kind -> Acc
-setKind acc kind =
+setKind : Kind -> Acc -> Acc
+setKind kind acc =
     { acc | kind = kind }
 
 
@@ -66,19 +75,46 @@ parseStep char acc =
     case acc.kind of
         Initial ->
             if Char.isDigit char then
-                charToIntOr0 char
-                    |> ExpectDigit
-                    |> setKind acc
+                acc
+                    |> setKind (charToIntOr0 char |> ExpectDigit)
+
+            else
+                case char of
+                    '(' ->
+                        acc
+                            |> setKind (ExpectX 0)
+
+                    _ ->
+                        acc
+
+        ExpectX number ->
+            if Char.isDigit char then
+                acc
+                    |> setKind (ExpectX (number * 10 + charToIntOr0 char))
 
             else
                 case char of
                     _ ->
                         acc
+                            |> setKind (ExpectY number 0)
+
+        ExpectY xNumber yNumber ->
+            if Char.isDigit char then
+                acc
+                    |> setKind (ExpectY xNumber (yNumber * 10 + charToIntOr0 char))
+
+            else
+                case char of
+                    ')' ->
+                        appendPoint xNumber yNumber acc
+
+                    _ ->
+                        acc
 
         ExpectDigit number ->
             if Char.isDigit char then
-                ExpectDigit (number * 10 + charToIntOr0 char)
-                    |> setKind acc
+                acc
+                    |> setKind (ExpectDigit (number * 10 + charToIntOr0 char))
 
             else
                 case char of
@@ -94,6 +130,13 @@ appendNumber number acc =
       -- TODO Calling List.length here is kinda dumb perfwise. Storing
       -- the length as an int would be a Big-O improvement
         | output = Point (List.length acc.output |> toFloat) (toFloat number) :: acc.output
+        , kind = Initial
+    }
+
+
+appendPoint xNumber yNumber acc =
+    { acc
+        | output = Point (toFloat xNumber) (toFloat yNumber) :: acc.output
         , kind = Initial
     }
 
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -9,11 +9,36 @@ suite : Test
 suite =
     describe "Parse"
         [ describe "Parse.parse"
-            [ test "comma separated numbers" <|
+            [ test "bare numbers" <|
+                \_ ->
+                    "1 22 333"
+                        |> Parse.parse
+                        |> expectOk [Point 0 1, Point 1 22, Point 2 333]
+            , test "comma separated numbers" <|
                 \_ ->
                     "1, 22, 333"
                         |> Parse.parse
                         |> expectOk [Point 0 1, Point 1 22, Point 2 333]
+            , test "comma separated numbers with brackets" <|
+                \_ ->
+                    "[1, 22, 333]"
+                        |> Parse.parse
+                        |> expectOk [Point 0 1, Point 1 22, Point 2 333]
+            , test "comma separated ordered tuples" <|
+                \_ ->
+                    "(0, 1), (1, 22), (2, 333)"
+                        |> Parse.parse
+                        |> expectOk [Point 0 1, Point 1 22, Point 2 333]
+            , test "comma separated unordered tuples" <|
+                \_ ->
+                    "(2, 1), (1, 22), (0, 333)"
+                        |> Parse.parse
+                        |> expectOk [Point 0 333, Point 1 22, Point 2 1]
+            , test "sparse comma separated unordered tuples" <|
+                \_ ->
+                    "(4, 1), (1, 22), (8, 333)"
+                        |> Parse.parse
+                        |> expectOk [Point 1 22, Point 4 1, Point 8 333]
             ]
         ]
 
